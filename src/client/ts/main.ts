dea7b3b53ac1b724cd2b461509a1ae3c5d2d2eba
Alex Sofin <alexsofin@Alexs-MacBook-Air.local>
2019-02-17 16:36:52 +0200

    added sounds to assets

---
+++ src/client/ts/main.ts
@@ -0,0 +1,4 @@
+import {ArrayList} from "ts-collections/collection/ArrayList"
+import { Collection } from "ts-collections/collection/Collection";
+let ksdnf: ArrayList<string> = Collection.emptyList<string>();
+ksdnf.add("a");
