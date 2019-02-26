234106390427bc68069ea165420ed7cde857a4b8
Алексей Софьин <alexplex236@gmail.com>
2019-02-26 14:26:22 +0200

    fixed last commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: "development",
+  entry: "./src/client/ts/main.js",
+  output: {
+    path: path.resolve(__dirname, 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.tsx', ".js", ".json"]
+  }
+}
+
+module.exports = config;
