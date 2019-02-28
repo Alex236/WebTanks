cfaff176bb0d9eb8a1ecf57c25bfbac22bd7a4d5
margarita <margarita.andriukhina@gmail.com>
2019-02-28 14:19:30 +0200

    fix index

--- src/client/ts/main.ts
+++ src/client/ts/main.ts
@@ -3,10 +3,19 @@ import { MapToGamingField } from "./Calculate/MapToGamingField";
 import { Map } from "./Calculate/Map";
 import { Events } from "./EventHandler/Events";
 import { Tank } from "./Calculate/Tank"
+import { Grid } from "./view/GridView";
+import { Rounds } from "./view/Rounds";
+
+let rounds = new Rounds();
+let rOne = rounds.roundOne;
+let tanksCoord = new Array(0,0)
+let position = "down"
 
 let gameProcessor = new GameProcessor(Map.getField());
 let tanks: Tank[] = gameProcessor.getTanks();
 
+let myGrid = new Grid();
+var x = 0;
 setInterval(() => {
     gameProcessor.calculate();
 
@@ -14,5 +23,10 @@ setInterval(() => {
     let y: number = tanks[0].getTankCoordinates().getY();
     let x: number = tanks[0].getTankCoordinates().getX();
     console.log(x + " " + y);
-    //add view
+    
+    myGrid.drawGrid(rOne);
+    myGrid.drawBase(6,12);
+    myGrid.drawTank(x,tanksCoord[1],tanksCoord[0],position);
+    x++;
+
 }, 250);
