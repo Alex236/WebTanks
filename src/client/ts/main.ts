3dd53d0a37ee43bee5c551130fe845fad054e735
margarita <margarita.andriukhina@gmail.com>
2019-03-01 22:29:49 +0200

    draw in one class but drawing parametrs also in main for now

--- src/client/ts/main.ts
+++ src/client/ts/main.ts
@@ -1,10 +1,10 @@
 import { GameProcessor } from "./Calculate/GameProcessor";
 import { Map } from "./Calculate/Map";
 import { Events } from "./EventHandler/Events";
-import { Tank } from "./Calculate/Tank"
-import { Grid } from "./view/GridView";
+import { Tank } from "./Calculate/Tank";
 import { Rounds } from "./view/Rounds";
 import { TankMove } from "./Calculate/enums/TankMove"
+import { draw } from "./View/draw";
 
 let rounds = new Rounds();
 let rOne = rounds.roundOne;
@@ -14,23 +14,23 @@ let position = "down"
 let gameProcessor = new GameProcessor(rOne);
 let tanks: Tank[] = gameProcessor.getTanks();
 
-let myGrid = new Grid();
 var iter = 0;
+
+//let drawAll = new draw(roundMap, tankCoordinate, position, iterForAnimation);
+let drawAll = new draw(rOne, tanksCoord, position, iter);
+
 setInterval(() => {
     gameProcessor.calculate();
-    console.log("loop");
+
     //logs
     let y: number = tanks[0].getTankCoordinates().getY();
     let x: number = tanks[0].getTankCoordinates().getX();
     let move: TankMove = tanks[0].getMove();
 
-    console.log(x + " " + y);
-    //add view
 
-    
-    myGrid.drawGrid(rOne);
-    myGrid.drawBase(6,12);
-    myGrid.drawTank(iter,x,y,position);
+    console.log(move);
+
+    drawAll.run(rOne, [x,y], position, iter);
 
     iter++;
 }, 200);
