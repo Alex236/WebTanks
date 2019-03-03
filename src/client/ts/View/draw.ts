77ba976746b722b186e605983bc8a377a796bf65
margarita <margarita.andriukhina@gmail.com>
2019-03-03 11:27:31 +0200

    add bullets drawing with turn

--- src/client/ts/View/draw.ts
+++ src/client/ts/View/draw.ts
@@ -1,16 +1,23 @@
-import { Grid } from "./GridView";
-import { User } from "../Models/User"
+import { User } from "../Models/User";
 import { Bullet } from "../Models/Bullet";
+import { DrawingElements } from "./DrawingElements"
+import { Base } from "./DrawingElements/Base";
+import { Grid } from "./DrawingElements/Grid";
+import { Tanks } from "./DrawingElements/Tanks";
+import { Bullets } from "./DrawingElements/Bullets";
 
 export class Draw{
-    private myGrid: Grid = new Grid();
+    private drawingElements: DrawingElements = new DrawingElements();
+    private base: Base = new Base(6,12);
+    private grid: Grid = new Grid();
+    private tanks: Tanks = new Tanks();
+    private bullets: Bullets = new Bullets();
 
     public run(map: number[][], allUsers: User[], allBullets: Bullet[]){
-        this.myGrid.drawGrid(map);
-        this.myGrid.drawBase(6,12);
-
-        this.myGrid.drawAllTanks(allUsers);
-        this.myGrid.drawAllBullets(allBullets);
+        this.grid.draw(map);
+        this.base.draw();
+        this.tanks.draw(allUsers);
+        this.bullets.draw(allBullets);
     }
 
 }
