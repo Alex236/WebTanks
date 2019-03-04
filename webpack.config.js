35eafbe1448ef9a4fad7feecacff2d4c76322e9a
margarita <margarita.andriukhina@gmail.com>
2019-03-04 08:51:59 +0200

    global settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,21 @@
-const webpack = require('webpack');
+//const webpack = require('webpack');
 const path = require('path');
 
 const config = {
   mode: "development",
-  entry: "./src/client/ts/main.js",
-  output: {
-    path: path.resolve(__dirname, 'js'),
-    filename: 'bundle.js'
-  },
+  entry: "./src/client/ts/main.ts",
   module: {
     rules: [
       {
         test: /\.(ts|tsx)?$/,
         loader: 'ts-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              mimetype: 'image/png'
-            }
-          }
-        ]
       }
     ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   resolve: {
     extensions: [ '.ts', '.tsx', ".js", ".json"]
