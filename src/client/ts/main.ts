07e1ce991497aea91ce35babcb308f63f8ed667c
margarita <margarita.andriukhina@gmail.com>
2019-03-06 15:07:42 +0200

    some little changes

--- src/client/ts/main.ts
+++ src/client/ts/main.ts
@@ -1,9 +1,9 @@
 import { Game } from "./Game";
-import { Area } from "./Models/Area";
+import { Arena } from "./Models/Arena";
 import { Tank } from "./Models/Tank";
 import { TankType } from "./Models/enums/TankType";
 import { Vector } from "./Models/enums/Vector";
 
 
-let game = new Game([new Tank(TankType.user, Vector.up), new Tank(TankType.enemy, Vector.up)], new Area);
+let game = new Game([new Tank(TankType.user, Vector.up), new Tank(TankType.enemy, Vector.up)], new Arena);
 game.start();
