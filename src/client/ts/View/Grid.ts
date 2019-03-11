306912a51a196cffc36c6f6bafcfa39682d7e129
margarita <margarita.andriukhina@gmail.com>
2019-03-11 10:51:52 +0200

    settings in view

--- src/client/ts/View/Grid.ts
+++ src/client/ts/View/Grid.ts
@@ -6,40 +6,40 @@ import { TankType } from "../models/tank-type";
 export class Grid {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
-  private totalHeight: number = window.innerHeight;
-  private totalWidth: number = window.innerWidth;
-  private gameSize: number = this.totalHeight <= this.totalWidth ? (this.totalHeight - this.totalHeight * 0.1) : (this.totalWidth - this.totalWidth * 0.1);
+  private totalHeight: number = document.getElementById('arena')!.offsetHeight;
+  private totalWidth: number = document.getElementById('arena')!.offsetWidth;
+  private gameSize: number = this.totalHeight <= this.totalWidth ? this.totalHeight : this.totalWidth;
   private cellSize: number = this.gameSize / 52;
   private img: HTMLImageElement | any = document.getElementById("img") ;
 
   constructor() {
     this.canvas = <HTMLCanvasElement>document.getElementById('canvas');
-    this.canvas.width = this.totalWidth;
-    this.canvas.height = this.totalHeight;
+    this.canvas.width = this.gameSize;
+    this.canvas.height = this.gameSize;
     this.ctx = <CanvasRenderingContext2D>this.canvas.getContext("2d");
   };
 
   drawUnit(x : number, y : number, unit: number, turn: number = 0, type: number = 0):void { 
     switch(unit){
       case Units.Base : 
-        this.ctx.drawImage(this.img, 0, 0, 64, 64, (x * 4 * this.cellSize) + this.totalWidth * 0.05, (y * 4 * this.cellSize) + this.totalHeight * 0.05, this.cellSize * 4, this.cellSize * 4);
+        this.ctx.drawImage(this.img, 0, 0, 64, 64, x * 4 * this.cellSize, y * 4 * this.cellSize, this.cellSize * 4, this.cellSize * 4);
         break;
       case Units.Brick :
-        this.ctx.drawImage(this.img, 0, 1208, 57, 57, x * this.cellSize + this.totalWidth * 0.05, y * this.cellSize + this.totalHeight * 0.05, this.cellSize + 1, this.cellSize + 1);
+        this.ctx.drawImage(this.img, 0, 1208, 57, 57, x * this.cellSize, y * this.cellSize, this.cellSize + 1, this.cellSize + 1);
         break;
       case Units.HardBrick : 
-        this.ctx.drawImage(this.img, 0, 1152, 57, 57, x * this.cellSize + this.totalWidth * 0.05, y * this.cellSize + this.totalHeight * 0.05, this.cellSize + 1, this.cellSize + 1);
+        this.ctx.drawImage(this.img, 0, 1152, 57, 57, x * this.cellSize, y * this.cellSize, this.cellSize + 1, this.cellSize + 1);
         break;
       case Units.Green :
-        this.ctx.drawImage(this.img, 0, 64, 57, 57, x * this.cellSize + this.totalWidth * 0.05, y * this.cellSize + this.totalHeight * 0.05, this.cellSize + 1, this.cellSize + 1);
+        this.ctx.drawImage(this.img, 0, 64, 57, 57, x * this.cellSize, y * this.cellSize, this.cellSize + 1, this.cellSize + 1);
         break;
       case Units.Tank :
         let startPoint = this.setTrueType(type);
-        this.ctx.drawImage(this.img, 0, startPoint + 64*turn, 64, 64, x * this.cellSize + this.totalWidth * 0.05, y * this.cellSize + this.totalHeight * 0.05, this.cellSize * 4, this.cellSize * 4);    
+        this.ctx.drawImage(this.img, 0, startPoint + 64*turn, 64, 64, x * this.cellSize, y * this.cellSize, this.cellSize * 4, this.cellSize * 4);    
         break;
       case Units.Bullet :
         let a = turn <= 1 ? 1264 : 1296;
-        this.ctx.drawImage(this.img, 32*(turn%2), a, 32, 32,  (x * this.cellSize + this.cellSize/2) + this.totalWidth * 0.05, (y * this.cellSize + this.cellSize/2) + this.totalHeight * 0.05, this.cellSize, this.cellSize);
+        this.ctx.drawImage(this.img, 32*(turn%2), a, 32, 32,  x * this.cellSize + this.cellSize/2, y * this.cellSize + this.cellSize/2, this.cellSize, this.cellSize);
         break;
     } 
   }
@@ -62,7 +62,7 @@ export class Grid {
 
   drawRoad():void {
     this.ctx.fillStyle = '#000'
-    this.ctx.fillRect(this.totalWidth * 0.05, this.totalHeight * 0.05, this.gameSize, this.gameSize)
+    this.ctx.fillRect(0, 0, this.gameSize, this.gameSize)
   };
 
   draw(map: number[][], tanks: Tank[], bullets: Bullet[]):void {
@@ -91,7 +91,7 @@ export class Grid {
       }
 
       this.ctx.font = "30pt Arial";
-      this.ctx.fillText(String(tank.lifes), (tank.x + 1)  * this.cellSize + this.totalWidth * 0.05, tank.y * this.cellSize + this.totalHeight * 0.05);
+      this.ctx.fillText(String(tank.lifes), (tank.x + 1)  * this.cellSize, tank.y * this.cellSize);
       this.drawUnit(tank.x, tank.y, Units.Tank, tank.vector, tank.type);
     }
   };
