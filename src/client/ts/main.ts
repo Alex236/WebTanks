62cfc21e2477a95a6d1eb769a48bc4882c9b5d84
margarita <margarita.andriukhina@gmail.com>
2019-03-28 05:34:17 +0200

    little grid

--- src/client/ts/main.ts
+++ src/client/ts/main.ts
@@ -2,7 +2,19 @@ import { Game } from "./game";
 import { Arena } from "./models/arena";
 import { TankFactory } from "./tank-factory";
 
-let arena = require("./rounds/second.json");
+const buttons = document.querySelectorAll("#rounds li");
+buttons.forEach(button => {
+    button.addEventListener('click', function(this: HTMLLIElement, event: Event){
+        if(this && this.nodeName == "LI") {
+            if(true){ //здесь проверка на то,
+                //запущена ли игра или нет, если запущена,
+                //то удалить ее и создать новую с той картой,
+                //которая была выбрана по клику.
+            }
+            let arena = require('./rounds/' + this.getAttribute("data") + '.json');
+                let game = new Game([(new TankFactory).createTanks(), (new TankFactory).createTanks()], new Arena(arena.blocks));
+                game.start();
+        }
+    })
+}) ;
 
-let game = new Game([(new TankFactory).createTanks(), (new TankFactory).createTanks()], new Arena(arena.blocks));
-game.start();
