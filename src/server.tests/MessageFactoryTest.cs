693baa1b450e289aaf3f9a170c79223c0331428b
Little Celestia <alexplex236@gmail.com>
2019-05-08 18:20:57 +0300

    deleted server.mock

--- src/server.tests/MessageFactoryTest.cs
+++ src/server.tests/MessageFactoryTest.cs
@@ -3,16 +3,27 @@ using Xunit;
 using WebTanksServer;
 using System.Collections.Generic;
 using System.Diagnostics;
-using server.mock;
+using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace WebTanksServer.Tests
 {
     public class GameControllerTest
     {
+        private int timer = 3000;
+        private string startNode = "node ../server/wwwroot/controller.bundle.js";
+        private string key = "pii43u2ibpibp34buikjn";
         [Fact]
         public void MainGameControllerTest()
-        {            
-            //Assert.Equal(res, 0);
+        {  
+            Process process = new Process();
+            process.StartInfo.FileName = "/bin/bash";
+            process.StartInfo.Arguments = "-c \" " + startNode + " " + key + " \"";
+            process.Start();
+            bool result = true;
+            result = process.WaitForExit(1000);  
+            Assert.True(result);
         }
     }
 }
